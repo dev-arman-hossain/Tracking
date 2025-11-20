56efab300cde894a92312e75d5dbeb13bdcfd14d
Arman Hossain <web.devarman@gmail.com>
2025-11-21 03:14:30 +0600

    Basic setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
